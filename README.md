b62c51b350edc1b2904026ea4460b57ba551eeb9
Gusthavo Loureiro <gusthavo.loureiro@mandic.net.br>
2021-04-21 23:08:52 -0300

    Base de dados com Terraform

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # terraform-base-de-dados
+
+* Código utilizando Terraform para instalar uma base de dados e criar seus usuários.
+
+* níveis de acesso:
+
+| usuario | acesso |
+|:---------:|:--------:|
+| app | leitura em todas as tabelas |
+| api | Criação em todoas as tabelas |
+| convidado | leitura nas tabelas da aplicacao |
+
+
+
---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+  version = ">= 3.1.0"
+  region  = var.region
+}
+provider "mysql" {
+  endpoint = aws_db_instance.database.endpoint
+  username = var.user_username
+  password = var.password_username
+}
+resource "aws_db_instance" "database" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  identifier           = "gusthavo"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "dbgusthavo"
+  username             = var.user_username
+  password             = var.password_username
+  db_subnet_group_name = "teste"
+  parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = ["sg-0a22bc564c00ca591"]
+  publicly_accessible = true
+}
+resource "mysql_database" "aplicacao" {
+  name = "aplicacao"
+}
+resource "mysql_database" "logs" {
+  name = "logs"
+}
+resource "mysql_user" "app" {
+  user               = "app"
+  host               = "%"
+  plaintext_password = var.passwd
+}
+resource "mysql_grant" "app" {
+  user       = mysql_user.app.user
+  host       = mysql_user.app.host
+  database   = "aplicacao"
+  privileges = ["SELECT"]
+}
+resource "mysql_user" "api" {
+  user               = "api"
+  host               = "%"
+  plaintext_password = var.passwd_api
+}
+resource "mysql_grant" "api" {
+  user       = mysql_user.api.user
+  host       = mysql_user.api.host
+  database   = "aplicacao"
+  privileges = ["INSERT"]
+}
+resource "mysql_user" "convidado" {
+  user               = "convidado"
+  host               = "%"
+  plaintext_password = var.passwd_convidado
+}
+resource "mysql_grant" "convidado" {
+  user       = mysql_user.convidado.user
+  host       = mysql_user.convidado.host
+  database   = "aplicacao"
+  privileges = ["SELECT"]
+}
